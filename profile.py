b9406d5f142094911c431c5a27c1bfad428d7f92
AkshayDabherao <cs12b1005@iith.ac.in>
2015-11-26 22:38:49 +0530

    profile eye support

--- profile.py
+++ profile.py
@@ -12,24 +12,26 @@ if len(sys.argv) > 1:
             print 'Failed to load address:', address
             sys.exit(1)
 else:
-    	address = './data/eye01.jpg'
+    	address = './natalie.jpg'
 
 img = cv2.imread(address)
-face_cascade = cv2.CascadeClassifier('/home/akshay/Desktop/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/home/akshay/Desktop/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_profileface.xml')
+eye_cascade = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_mcs_lefteye.xml')
 
 def detectFaces(img):
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=4)
+	print faces
 	for (x,y,w,h) in faces:
 		cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
 		roi_gray = gray[y:y+h, x:x+w]
 		
 	eyes = eye_cascade.detectMultiScale(gray)
+	print eyes
 	for (a,b,c,d) in eyes:
 		cv2.rectangle(gray,(a,b),(a+c,b+d),(255,0,0),2)
 		roi_eye = gray[b:b+d,a:a+c]
-	return gray#,roi_eye
+	return gray
 
 
 ans  = detectFaces(img)
--- test.py
+++ test.py
@@ -9,8 +9,8 @@ print len(obj)
 
 #img = cv2.imread(address)
 face_cascade = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
-
+eye_cascade = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+face_profile = cv2.CascadeClassifier('../opencv-3.0.0/data/haarcascades/haarcascade_profileface.xml')
 def detectFaces(img):
 	gray = np.copy(img)#cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=4)
@@ -24,7 +24,21 @@ def detectFaces(img):
 		roi_eye = gray[b:b+d,a:a+c]
 	return gray#,roi_eye
 
+def detectProfile(img):
+	gray = np.copy(img)#cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces = face_profile.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=4)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+		roi_gray = gray[y:y+h, x:x+w]
+	print "called"
+	eyes = eye_cascade.detectMultiScale(gray)
+	for (a,b,c,d) in eyes:
+		cv2.rectangle(gray,(a,b),(a+c,b+d),(255,0,0),2)
+		roi_eye = gray[b:b+d,a:a+c]
+	return gray#,roi_eye
+
 lol = detectFaces(obj[0])
+lol2 = detectProfile(obj[34])
 plt.subplot(121),plt.imshow(lol,'gray'),plt.title('myharris')
-plt.subplot(122),plt.imshow(obj[30],'gray'),plt.title('inbuilt harris')
+plt.subplot(122),plt.imshow(lol2,'gray'),plt.title('inbuilt harris')
 plt.show()
